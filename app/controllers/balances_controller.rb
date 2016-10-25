be018daefae53cf3311d5bf885b0c1641f07b4a7
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-25 04:44:01 +0000

    new record box created

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -1,8 +1,9 @@
 class BalancesController < ApplicationController
     def index
         @balances = Balance.all
+        @balance = Balance.new
     end
     def new
-        @balance = Balance.new
+        
     end
 end
