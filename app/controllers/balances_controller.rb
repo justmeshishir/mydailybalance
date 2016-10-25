0139c8388dca45499a3f65c420a69bf4b4124297
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-25 09:37:32 +0000

    edit and delete created

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -8,6 +8,22 @@ class BalancesController < ApplicationController
         redirect_to root_path
     end
     
+    def edit
+        @balance = Balance.find(params[:id])
+    end
+    def update
+      @balance = Balance.find(params[:id])
+      if @balance.update(balance_params)
+        redirect_to root_path
+      else
+        redirect_to edit_balance_path(params[:id])
+      end
+    end
+    def destroy
+        @balance = Balance.find(params[:id])
+        @balance.destroy
+        redirect_to root_path
+    end
     private
     def balance_params
         params.require(:balance).permit(:title, :amount, :date)
