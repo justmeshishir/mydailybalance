aee7a044015f68226fdea8e14cb49e491616802a
Shishir Thapa <shishirthapa1@gmail.com>
2016-10-25 11:24:59 +0000

    bugs fixed

--- app/controllers/balances_controller.rb
+++ app/controllers/balances_controller.rb
@@ -5,23 +5,33 @@ class BalancesController < ApplicationController
     end
     def create
         @create_balance = Balance.create(balance_params)
+        if @create_balance.valid?
+            flash[:success]= "Added successfully!"
+            
+        else
+            flash[:error]= "Sorry, it couldnot be added. Try Again!"
+        end
         redirect_to root_path
     end
     
     def edit
         @balance = Balance.find(params[:id])
+        
     end
     def update
       @balance = Balance.find(params[:id])
       if @balance.update(balance_params)
+          flash[:success]= "Edited successfully!"
         redirect_to root_path
       else
+        flash[:error]= "Sorry, it couldnot be added. Try Again!"
         redirect_to edit_balance_path(params[:id])
       end
     end
     def destroy
         @balance = Balance.find(params[:id])
         @balance.destroy
+        flash[:success]= "Deleted successfully!"
         redirect_to root_path
     end
     private
